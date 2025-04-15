a0d384683d4ee2d57636c1515fa3097aaa8b5e9d
Marcus Milantoni <Marcus.Milantoni@lhsc.on.ca>
2025-04-15 12:22:48 -0400

    fix ConvNeXt

--- 3D_ConvNext_model/ConvNeXt3D.py
+++ 3D_ConvNext_model/ConvNeXt3D.py
@@ -1,10 +1,10 @@
 import torch
 from torch import nn, Tensor
 from torch.nn import functional as F
-from torch.ops.misc import Permute, Conv3dNormActivation
-from torch.ops.stochastic_depth import StochasticDepth
-from torch._api import register_model, WeightsEnum
-from torch._utils import _ovewrite_named_params
+from torchvision.ops.misc import Permute, Conv3dNormActivation
+from torchvision.ops.stochastic_depth import StochasticDepth
+from torchvision.models._api import register_model, WeightsEnum
+from torchvision.models._utils import _ovewrite_named_params
 
 from typing import Callable, Optional, Any, List, Sequence
 from functools import partial
